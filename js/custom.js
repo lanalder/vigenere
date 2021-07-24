0c4671593934b4d0d32506189bc6ec2c74a9c39d
lanalder <lalanalder@gmail.com>
2021-07-24 17:17:35 +1200

    actually readable code

--- js/custom.js
+++ js/custom.js
@@ -1,254 +1,204 @@
-// let v = 'DAZFI SFSPA VQLSN PXYSZ WXALC DAFGQ UISMT PHZGA MKTTF TCCFXKFCRG GLPFE TZMMM ZOZDE ADWVZ WMWKV GQSOH QSVHP WFKLS LEASEPWHMJ EGKPU RVSXJ XVBWV POSDE TEQTX OBZIK WCXLW NUOVJ MJCLLOEOFA ZENVM JILOW ZEKAZ EJAQD ILSWW ESGUG KTZGQ ZVRMN WTQSEOTKTK PBSTA MQVER MJEGL JQRTL GFJYG SPTZP GTACM OECBX SESCIYGUFP KVILL TWDKS ZODFW FWEAA PQTFS TQIRG MPMEL RYELH QSVWBAWMOS DELHM UZGPG YEKZU KWTAM ZJMLS EVJQT GLAWV OVVXH KWQILIEUYS ZWXAH HUSZO GMUZQ CIMVZ UVWIF JJHPW VXFSE TZEDF';
-
-// let v = 'TYWUR USHPO SLJNQ AYJLI FTMJY YZFPV EUZTS GAHTU WNSFW EEEVAMYFFD CZTMJ WSQEJ VWXTU QNANT MTIAW AOOJS HPPIN TYDDM VKQUFLGMLB XIXJU BQWXJ YQZJZ YMMZH DMFNQ VIAYE FLVZI ZQCSS AEEXVSFRDS DLBQT YDTFQ NIVKU ZPJFJ HUSLK LUBQV JULAB XYWCD IEOWHFTMXZ MMZHC AATFX YWGMF XYWZU QVPYF AIAFJ GEQCV KNATE MWGKXSMWNA NIUSH PFSRJ CEQEE VJXGG BLBQI MEYMR DSDHU UZXVV VGFXVJZXUI JLIRM RKZYY ASETY MYWWJ IYTMJ KFQQT ZFAQK IJFIP FSYAGQXZVK UZPHF ZCYOS LJNQE MVK';
-
-// let v = 'VVQGY TVVVK ALURW FHQAC MMVLE HUCAT WFHHI PLXHV UWSCI GINCMUHNHQ RMSUI MHWZO DXTNA EKVVQ GYTVV QPHXI NWCAB ASYYM TKSZRCXWRP RFWYH XYGFI PSBWK QAMZY BXJQQ ABJEM TCHQS NAEKV VQGYTVVPCA QPBSL URQUC VMVPQ UTMML VHWDH NFIKJ CPXMY EIOCD TXBJWKQGAN';
-
-// let v = 'QRBAI UWYOK ILBRZ XTUWL EGXSN VDXWR XMHXY FCGMW WWSME LSXUZMKMFS BNZIF YEIEG RFZRX WKUFA XQEDX DTTHY NTBRJ LHTAI KOCZXQHBND ZIGZG PXARJ EDYSJ NUMKI FLBTN HWISW NVLFM EGXAI AAWSLFMHXR SGRIG HEQTU MLGLV BRSIL AEZSG XCMHT OWHFM LWMRK HPRFBELWGF RUGPB HNBEM KBNVW HHUEA KILBN BMLHK XUGML YQKHP RFBELEJYNV WSIJB GAXGO TPMXR TXFKI WUALB RGWIE GHWHG AMEWW LTAELNUMRE UWTBL SDPRL YVRET LEEDF ROBEQ UXTHX ZYOZB XLKAC KSOHNVWXKS MAEPH IYQMM FSECH RFYPB BSQTX TPIWH GPXQD FWTAI KNNBXSIYKE TXTLV BTMQA LAGHG OTPMX RTXTH XSFYG WMVKH LOIVU ALMLDLTSYV WYNVW MQVXP XRVYA BLXDL XSMLW SUIOI IMELI SOYEB HPHNRWTVUI AKEYG WIETG WWBVM VDUMA EPAUA KXWHK MAUPA MUKHQ PWKCXEFXGW WSDDE OMLWL NKMWD FWTAM FAFEA MFZBN WIHYA LXRWK MAMIKGNGHJ UAZHM HGUAL YSULA ELYHJ BZMSI LAILH WWYIK EWAHN PMLBNNBVPJ XLBEF WRWGX KWIRH XWWGQ HRRXW IOMFY CZHZL VXNVI OYZCMYDDEY IPWXT MMSHS VHHXZ YEWNV OAOEL SMLSW KXXFX STRVI HZLEFJXDAS FIE';
-
-// let v = 'LPROZ OOGRJ ZGFLV TUKMC WFDQM PZXIJ LVRWQ XEOSZ ZHTEK UYSCRPTFCZ UHXIJ LPPTD CPRBY OSMGY TLEVD UAQMF IFMZV LVYTO QDLHXLBPLL KYCQY ODRKS ACTEU XZEVO UAQMF OSDSU BKMBJ QEORF WFQCKHKSOD INGJZ SHGVV LMSZD WWHFJ AVQGF NUWMW AOIXT CSRYC YPPTPLFUCR AVQHR RVRES QCKHM LGARF YHXZP CSNWS NCRQV GHRLR ZEDHFJVUPC XZJQC ATISQ SCGXN BALWY MAQCY OSD'
-
-// let v = 'FIIFL VZOZS VPDCA ZVFSL EMRUL BQISC XVQTS NDMFT IDGIZ ILZDMFFLVZ YMHCG DIGSL DSHEZ SIWMM XPNAN TIIRJ SFMWB XIDPS EWHAIXYWQM EXVVV DMRUK XASPF OQTUP JLNTQ WTJYQ OLFOF EOVVW WTURXDIGPT LLMFT INJYF OLKZU FXMVK CZISV AHDQQ VEVDM RTWIR MWYJIGPRFO CFUWK ZYFUQ VGZZU KYLNT MXKZY SDEMW MMXPX SJUZK NAXQQZVJSA ZICWN ERSIL BTUWJ HLUFI ZFNTQ GYMLO TARQJ MFLJL ISXMUWUZPA VXUUD MVKNT MXUGL GZFPL BQFVZ HFQTI TSNQE XVSGR DSDLBQBVVK YZOIF XNTQW LFZAX PFOCZ SHRJE ZQWJD CWQEU JYMYR FOUDQJIGFU ORFLU YAYJW MTMPC VCEFY ITNTU WYSFX AAUZI GEIZS GEQRKOCFTF IGIYN IWGLQ FSJOY QBXYW XGEXS WBUZH KZYPA SI'
+const btn = document.querySelector('.btn'),
+  showkey = document.querySelector('.tellkw'),
+  inp = document.querySelector('.inp');
 
 let kwl = 0,
+  // ^ keyword length, below plaintext ran-gend kwl
   ptkwl = [],
   table = [],
-  v,
-  of;
+  // v holds text
+  v;
 
 let omni = {
   abc: [],
+  // english letter freqs
   eng: [8.15, 1.44, 2.76, 3.79, 13.11, 2.92, 1.99, 5.26, 6.35, 0.13, 0.42, 3.39, 2.54, 7.10, 8.00, 1.98, 0.12, 6.83, 6.10, 10.47, 2.46, 0.92, 1.54, 0.17, 1.98, 0.08],
-  // get overflow() {
-  //   let decimal = (v.length / kwl) - Math.floor(v.length / kwl);
-  //   return Math.floor(v.length * (decimal / 10));
-  // },
+
   abcInit() {
+    // lazy way 2 get the alphabet
     for (let i = 97; i < 123; i++) {
       this.abc[i - 97] = String.fromCharCode(i);
     }
-  }
+  },
+
+  sum(arr) {
+    return arr.reduce((acc, val) => acc + val, 0); },
+
+  uniquify(arr) {
+    return Array.from(new Set(arr)); }
 };
 
 let patterns = {
-  allF: [],
-  factors: [[], []],
-  shifts(setSize) {
-    // chronologically 1.2, creates sets of length = setSize, returning each set of sets before shifting along 1 until setSize and returning that etc, since pattern repeats aren't gonna adhere to sets always sectioned from 0
-    let a = [];
-    let p = 0;
-    for (let sh = 0; sh < setSize; sh++) {
-      // 1st loop for shifting start of set (better data if not just sets of 3 from 0 taken but 3 from 1 etc), 2nd sets those sets in2 sets :O + p is inc to determine start, i is pos in sets
-      for (let i = 0; i < v.length / setSize - 1; i++) {
-        a.push(v.substring(i * setSize + p, i * setSize + setSize + p));
+  factors: [],
+
+  shifts(sectSize) {
+    let offSect = [];
+
+    for (let sh = 0; sh < sectSize; sh++) {
+
+      for (let i = 0; i < v.length / sectSize; i++) {
+        offSect.push(v.substring(i * sectSize + sh, i * sectSize + sectSize + sh));
+        // intentionally not i * (sectSize * 2) in second half, ig bedmas can prove useful?? this way spaced out evenly so that substring taken from one end of sectSize to the other, + sh to account for offset from start, vs. exponentially jumping greater & greater distances between substrings
       }
-      p++;
     }
-    return a;
+
+    return offSect;
   },
-  countRPs(set) {
-    // chronologically 1.3, finds n counts repeats
+
+  factor(arrDist) {
+
+    // find factors of all distances between rps
+    for (let i = 0; i < arrDist.length; i++) {
+
+      // start from 3 since 2 is cluttering and unlikely kwl
+      let dividing = 3;
+      let handful = [];
+
+      // for sake of performance will assume kwl < 25
+      while (dividing < 20) {
+
+        // if not a factor or alr been found, increase dividing number
+        if (arrDist[i] % dividing || dividing === handful[handful.length - 1]) {
+          dividing += 1;
+        } else {
+          handful.push(dividing);
+          // again for performance, only test first 3 factors
+          if (handful.length > 3) {
+            dividing = 20;
+          }
+        }
+      }
+
+      // if no factors < 25 are found, can assume kwl might be just that distance
+      if (!handful.length && arrDist[i] < 100) {
+        handful = [arrDist[i]];
+      }
+
+      this.factors.push(...handful);
+      // store found factors n delete duplicates in uniquify
+      this.factors = omni.uniquify(this.factors);
+    }
+  },
+
+  repeats(sectSize) {
+    // finds repeating patterns of sectSize-length (parameter from keycutter() loop between 3-6), sets holds text in sectSize chunks
+    let sets = this.shifts(sectSize);
+    // gaps 2d so 0 can hold actual ind of pattern in v, while 1 = dist between rps
+    let gaps = [[], []];
     let occr = [];
-    let no = new Map();
 
-    set.forEach((x) => {
-      let i = set.indexOf(x);
-      // rp patterns go into occr as many times as they occur, Map so each pattern count can b counted / only set once (i feel like there was a way 2 do this w arrs?? but i read it n then forgot it??)o:
-      if (set.indexOf(x, i + 1) !== -1) {
-        occr.push(x);
-        no.set(x, 0);
+    sets.forEach((morsel) => {
+      let rp = sets.filter(others => others === morsel);
+      // rp & filter lets us know if pattern repeats
+
+      if (rp.length > 1 && !occr.includes(morsel)) {
+        // occr just there so a repeating pattern's indices is pushed only once & not every time it shows up
+        occr.push(morsel);
+        gaps[0].push(v.indexOf(morsel));
+
+        // for as many times as pattern repeats (as found by rp filter) gaps[0] gets index of v so that the dist between rps can be taken n stored in gaps[1]
+        for (let i = 0; i < rp.length - 1; i++) {
+          gaps[0].push(v.indexOf(morsel, gaps[0][gaps[0].length - 1] + 1));
+          gaps[1].push(gaps[0][i + 1] - gaps[0][i]);
+        }
+
+        gaps[0] = [];
       }
     });
 
-    occr.sort();
-    // sort (default 2 alphabetically) so can just compare neighbours
-    for (let i = 0; i < occr.length; i++) {
-
-      if (occr[i] === occr[i + 1]) {
-        let c = no.get(occr[i]);
-        no.set(occr[i], c + 1);
-      }
-    }
-    return no;
-  },
-
-  findRPs(setSize) {
-    // chronologically, no.1; finds repeating patterns of lengths 3, 4, 5, 6 (length determined by setSize, which is determined by loop in keycutter)
-    let sets = this.shifts(setSize);
-    let gaps = [];
-
-    Array.from(this.countRPs(sets).keys()).forEach((x) => {
-      gaps.push(v.indexOf(x));
-      // push ind of rp set, n from that ind of sets (last ele of gaps from above, -1) push next (+1) ind of same rp until no more tb found
-      while (v.indexOf(x, gaps[gaps.length - 1] + 1) !== -1) {
-        gaps.push(v.indexOf(x, gaps[gaps.length - 1] + 1));
-      }
-    });
-
-    let m = Array.from(this.countRPs(sets).values());
-    // finds not the indices of repeats but the dist between them
-    for (let i = 0; i < gaps.length; i++) {
-
-      for (let j = 0; j < m[i]; j++) {
-        gaps[i + j] = gaps[i + j + 1] - gaps[i + j];
-        gaps.splice(i + j + 1, 1);
-      }
-    }
-
-    for (let i = 0; i < gaps.length; i++) {
-      factor(gaps[i]);
-    }
-
-    // counts n stores factors that occur enough (more than once lol still keeping options open at this point) 2 b plausible
-    this.allF = this.allF.flat();
-    this.allF.forEach((x) => {
-      let occr = this.allF.filter(y => y === x);
-
-      if (!this.factors[0].includes(x) && occr.length > 1) {
-        this.factors[0].push(x);
-        this.factors[1].push(occr.length);
-      }
-    });
-    this.allF = [];
+    gaps[1] = omni.uniquify(gaps[1]);
+    this.factor(gaps[1]);
   }
 };
 
-function factor(x) {
-  // chronologically 1.4, having found rp patterns find common factors of dist between them
-  let dv = 3;
-
-  let few = [];
-
-  while (dv < 20) {
-    if (x % dv || dv === few[few.length - 1]) {
-      dv += 1;
-    } else {
-      few.push(dv);
-      if (few.length > 3) {
-        dv = 20;
-      }
-    }
-  }
-  if (!few.length) {
-    few = [x];
-  }
-  patterns.allF.push(few);
-}
-
 let freqkey = {
   ic: [[], []],
+  // holds ic calcs for each letter
+  formIC: set26(),
   chi: [],
-  bigBoyIC: [0, 0, 0],
 
-  calcIC() {
-    // chronologically 2.1, for each coset count how many times each letter shows up, then do the formula thing, sum, push
-    let form = set26();
+  calcIC(text) {
+    // this & below method r seperate since 2 determine if user input is plaintext or ciphered will deal with full v text or table coset respectively (arg must be array)
+    let occr = set26();
 
-    for (let i = 0; i < table.length; i++) {
-      let occr = set26();
-      for (let j = 0; j < table[i].length; j++) {
-        occr[omni.abc.indexOf(table[i][j])] += 1;
-      }
-      occr.forEach((x, l) => {
-        form[l] += (x / table[i].length) * ((x - 1) / (table[i].length - 1));
-      });
-    }
-
-    let icsum = 0;
-    form.forEach((x) => {
-      icsum += x;
+    // count each letter's occurance (occr ind = letter)
+    text.forEach((lett) => {
+      occr[omni.abc.indexOf(lett)] += 1;
     });
 
-    this.ic[0].push(kwl);
-    this.ic[1].push(icsum / kwl);
+    // ic formula
+    occr.forEach((count, lett) => {
+      let len = text.length;
+      this.formIC[lett] += (count / len) * ((count - 1) / (len - 1));
+    });
   },
 
-  icButPlain() {
-    let occr = set26();
-    let form = set26();
+  deICpher() {
+    // for deciphering only, since here IC of full text is useless it's the cosets that matter
+    this.formIC = set26();
 
-    for (let i = 0; i < v.length; i++) {
-      occr[omni.abc.indexOf(v[i])] += 1;
+    // hence calc IC for each coset individually
+    for (let i = 0; i < kwl; i++) {
+      this.calcIC(table[i]);
     }
-    occr.forEach((x, l) => {
-      form[l] += (x / v.length) * ((x - 1) / (v.length - 1));
-    });
 
-    let icsum = 0;
-    form.forEach((x) => {
-      icsum += x;
-    });
-
-    return icsum;
+    this.ic[0].push(kwl);
+    this.ic[1].push(omni.sum(this.formIC) / kwl);
   },
 
   chiron() {
     for (let i = 0; i < kwl; i++) {
       let chi = set26();
 
-      chi = chi.map((x, l) => {
-        let fs = [];
+      chi = chi.map((abcSub, subAmt) => {
+        let formul = [];
 
-        let shifty = table[i].map((y) => {
-          let og = omni.abc.indexOf(y);
-          return omni.abc[Math.abs(26 + (og - l)) % 26];
+        let subbed = table[i].map((ele) => {
+          let og = omni.abc.indexOf(ele);
+          return omni.abc[Math.abs(26 + (og - subAmt)) % 26];
         });
 
-        shifty.forEach((a) => {
-          let letpos = omni.abc.indexOf(a);
-          let fe = omni.eng[letpos] / 100;
-          let occr = shifty.filter(y => y === a);
-          let dif = (occr.length / 10) - fe;
-          let top = Math.pow(dif, 2);
+        subbed.forEach((ele) => {
+          let letpos = omni.abc.indexOf(ele);
+          let occr = subbed.filter(x => x === ele);
+          let calcs = Math.pow((occr.length / 10) - (omni.eng[letpos] / 100), 2);
+          calcs = calcs / (omni.eng[letpos] / 100);
 
-          fs.push(top / fe);
-          shifty = shifty.filter(y => y !== a);
+          formul.push(calcs);
+          subbed = subbed.filter(x => x !== ele);
         });
 
-        let xsum = 0;
-        fs.forEach((y) => {
-          xsum += y;
-        });
-
-        return xsum;
+        return omni.sum(formul);
       });
 
-      chi.forEach((x) => {
-        // someone needs 2 take for loops away from me but lit how else would one do this...
-        let sml = chi.filter(y => y < x);
-        sml.sort((a, b) => {
-          return a - b;
-        });
-
-        this.chi[i] = chi.indexOf(sml[0]);
-      });
+      this.chi[i] = chi.indexOf(Math.min(...chi));
     }
   }
 };
 
 function keycutter() {
-  // v = v.replace(/[^\w\s]|_/g, " ").replace(/\s+/g, "").toLowerCase();
-  // omni.abcInit();
-  // 1. factors of kwl
+  // find common factors 2 narrow down plausible kwl
   for (let i = 3; i < 7; i++) {
-    patterns.findRPs(i);
+    patterns.repeats(i);
   }
-  // 2. set kwl 2 plausible options, ie. common factors (factors aren't enough to determine kwl as factors can b factors of each other yk); matrix having made table of kwl cosets begins finding incident coincident whatsit called the probability of 2 same letters being picked at random, which in coherent eng text is 0.06something whilst in gibbledegook more like 0.03 (if ciphertext on that assumed kwl has similar letter distribution / repeats to normal english (ie. bigger is better) then that kwl is probably the go)
-  for (let i = 0; i < patterns.factors[0].length; i++) {
-    kwl = patterns.factors[0][i];
+
+  // for kwl possibilities, find incident coincident thing (best fitting letter freq distribution compared 2 english (0.03: no way; 0.06: sensible language))
+  for (let i = 0; i < patterns.factors.length; i++) {
+    kwl = patterns.factors[i];
     matrix();
+    freqkey.deICpher();
     table = [];
   }
-  // 2.1 find biggest IC n with it have kwl determined (hopefully)!
-  freqkey.ic[1].forEach((x, i) => {
-    if (x > freqkey.bigBoyIC[2] || patterns.factors[1][i] >= freqkey.bigBoyIC[1]) {
-      freqkey.bigBoyIC = [freqkey.ic[0][i], patterns.factors[1][i], x];
-      // factor, occr count, ic
-    }
-  });
-  kwl = freqkey.bigBoyIC[0];
+
+  // kwl = biggest IC
+  let biggestIC = Math.max(...freqkey.ic[1]);
+  kwl = freqkey.ic[0][freqkey.ic[1].indexOf(biggestIC)];
+
+  // get new table of kwl cosets, calc chi2
   matrix();
   freqkey.chiron();
+
+  // sub in proper letters
   solve();
 }
 
@@ -261,21 +211,16 @@ function matrix() {
     table[i] = [];
 
     for (let j = 1; j < v.length / kwl; j++) {
-      // sometimes the
-      if (j) {
-        table[i].push(v[j * kwl - (kwl - i)]);
-      }
+      // not starting at 0 since j isn't used for arr index (and * 0 is problematic here), rather what kw letter each text letter lines up with
+      table[i].push(v[j * kwl - (kwl - i)]);
+      // j * kwl = relevant fraction of text (as human readable row, not coset), minus kwl - 1 = kw letter, = pos in text & kw
     }
 
     if (overflow) {
+      // first few cosets will be longer on account of tail-end remainder
       table[i].push(v[v.length - overflow]);
-      console.log(v[v.length - overflow]);
       overflow -= 1;
     }
-  }
-
-  if (!freqkey.bigBoyIC[0] && !ptkwl.length) {
-    freqkey.calcIC();
   }
 }
 
@@ -300,7 +245,7 @@ function solve() {
     });
   }
 
-  document.querySelector('.inp').value = unciphd.join(' ');
+  inp.value = unciphd.join(' ');
 }
 
 function set26() {
@@ -314,18 +259,21 @@ function set26() {
 
 // dom + ciphering stuff
 
-document.querySelector('.btn').addEventListener('click', function() {
-  document.querySelector('.tellkw').textContent = '';
+btn.addEventListener('click', function() {
+  showkey.textContent = '';
   omni.abcInit();
 
-  if (document.querySelector('.inp').value !== "") {
-    v = document.querySelector('.inp').value;
-
+  if (inp.value !== "") {
+    v = inp.value;
     v = v.replace(/[^\w\s]|_/g, " ").replace(/\s+/g, "").toLowerCase();
+
     if (v.length < 20) {
-      // tell them is better when longer, option 2 proceed tho
+      alert('reversing the cipher of text this short will probably just make a mess -- im not a wizard sadly but even if i were, id be hungry for a longer sample size thanks! <|:o)');
     }
-    if (freqkey.icButPlain() < 0.055) {
+
+    freqkey.calcIC(Array.from(v));
+    // can tell if user entered plaintext or ciphered from IC
+    if (omni.sum(freqkey.formIC) < 0.055) {
       keycutter();
       revealKW();
     } else {
@@ -336,13 +284,7 @@ document.querySelector('.btn').addEventListener('click', function() {
 }, false);
 
 function ciphed() {
-  let max = Math.ceil(v.length / 25);
-  if (max < 3) {
-    max = 5;
-  } else if (max > 18) {
-    max = 12;
-  }
-  for (let i = 0; i < Math.round(Math.random() * (max - 3) + 3); i++) {
+  for (let i = 0; i < Math.round(Math.random() * (12 - 5) + 5); i++) {
     ptkwl[i] = Math.round(Math.random() * 25);
   }
 
@@ -354,28 +296,23 @@ function ciphed() {
 
 function revealKW() {
   let rkw = '';
+
   if (ptkwl.length) {
-    ptkwl.forEach((x) => {
-      rkw += `${omni.abc[x]}`;
-      // is temp lit necessary?
-    });
+    ptkwl.forEach((x) => { rkw += omni.abc[x]; });
   } else {
-    // bug here: kw shouldn't rp
-    freqkey.chi.forEach((x) => {
-      rkw += `${omni.abc[x]}`;
-    });
+    freqkey.chi.forEach((x) => { rkw += omni.abc[x]; });
   }
-  document.querySelector('.tellkw').textContent = rkw;
+
+  showkey.textContent = rkw;
 }
 
 function reset() {
   kwl = 0;
   ptkwl = [];
   table = [];
-  v = null;
-  patterns.allF = [];
-  patterns.factors = [[], []];
+  v = ' ';
+  patterns.factors = [];
   freqkey.ic = [[], []];
+  freqkey.formIC = set26();
   freqkey.chi = [];
-  freqkey.bigBoyIC = [0, 0, 0];
 }
--- js/notcipher.js
+++ js/notcipher.js
@@ -1,6 +1,6 @@
 const mm = document.querySelector('.mouth');
 
-const txt = '<p>i, mr. vigenere, have made you a cipher! your secrets are safe with me and my wealth of alphabetical substitutions (provided you live between the 16th to 19th centuries) -- just confess something juicy in plain english there on the right.</p> <p>whats this? the code thats so kindly hosting me figures it can crack my cipher... blasphemy! best prove it wrong by entering some ciphertext in that same box over there, and if you strangely dont have on hand pre-vigenered paragraphs, <a href="https://pages.mtu.edu/~shene/NSF-4/Tutorial/VIG/Vig-Examples.html" target="_blank">you can find some here</a></p>';
+const txt = '<p>i, mr. vigenere, have made you a cipher! your secrets are safe with me and my many alphabetical substitutions (provided you live between the 16th to 19th centuries) -- just confess something juicy in plain english there on the right.</p> <p>whats this? the code thats so kindly hosting me figures it can crack my cipher... blasphemy! best prove it wrong by entering some ciphertext in that same box, and if weirdly you dont have on hand pre-vigenered paragraphs, <a href="https://pages.mtu.edu/~shene/NSF-4/Tutorial/VIG/Vig-Examples.html" target="_blank">you can find some here</a></p>';
 
 var speak = () => {
     anime({
